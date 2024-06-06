a0bbe70037d90cf26f63f32a5736197209aabd88
KsuhaBur <79745273+KsuhaBur@users.noreply.github.com>
2024-06-06 18:53:56 +1000

    add front

---
+++ frontend/app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
